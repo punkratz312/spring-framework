d50507780aa4e039a5382292617298edbb5fb742
Vincent Potucek <vincent.potucek@sap.com>
2025-02-25 09:35:08 +0100

    dead code

--- spring-beans/src/test/java/org/springframework/beans/factory/config/ServiceLocatorFactoryBeanTests.java
+++ spring-beans/src/test/java/org/springframework/beans/factory/config/ServiceLocatorFactoryBeanTests.java
@@ -140,14 +140,6 @@ class ServiceLocatorFactoryBeanTests {
 				.addPropertyValue("serviceLocatorInterface", TestServiceLocator3.class)
 				.getBeanDefinition());
 
-//		StaticApplicationContext ctx = new StaticApplicationContext();
-//		ctx.registerPrototype("testService", TestService.class, new MutablePropertyValues());
-//		ctx.registerAlias("testService", "1");
-//		MutablePropertyValues mpv = new MutablePropertyValues();
-//		mpv.addPropertyValue("serviceLocatorInterface", TestServiceLocator3.class);
-//		ctx.registerSingleton("factory", ServiceLocatorFactoryBean.class, mpv);
-//		ctx.refresh();
-
 		TestServiceLocator3 factory = (TestServiceLocator3) bf.getBean("factory");
 		TestService testBean1 = factory.getTestService();
 		TestService testBean2 = factory.getTestService("testService");
@@ -172,16 +164,6 @@ class ServiceLocatorFactoryBeanTests {
 				.addPropertyValue("serviceLocatorInterface", TestServiceLocator3.class)
 				.addPropertyValue("serviceMappings", "=testService1\n1=testService1\n2=testService2")
 				.getBeanDefinition());
-
-//		StaticApplicationContext ctx = new StaticApplicationContext();
-//		ctx.registerPrototype("testService1", TestService.class, new MutablePropertyValues());
-//		ctx.registerPrototype("testService2", ExtendedTestService.class, new MutablePropertyValues());
-//		MutablePropertyValues mpv = new MutablePropertyValues();
-//		mpv.addPropertyValue("serviceLocatorInterface", TestServiceLocator3.class);
-//		mpv.addPropertyValue("serviceMappings", "=testService1\n1=testService1\n2=testService2");
-//		ctx.registerSingleton("factory", ServiceLocatorFactoryBean.class, mpv);
-//		ctx.refresh();
-
 		TestServiceLocator3 factory = (TestServiceLocator3) bf.getBean("factory");
 		TestService testBean1 = factory.getTestService();
 		TestService testBean2 = factory.getTestService("testService1");
